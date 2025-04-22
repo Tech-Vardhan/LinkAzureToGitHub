03be7b73e106cdbaa65751088dd18f7fa7006aae
Vardhan <108137991+Tech-Vardhan@users.noreply.github.com>
2025-04-22 18:46:35 +0530

    Initial commit

---
+++ learningapp/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.FeatureManagement;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Configuration.AddAzureAppConfiguration(
+    options=> 
+    {
+        options.Connect("Endpoint=https://appconfing.azconfig.io;Id=KwVS;Secret=A6i45AywzvIvyJ3z6LOHFfkNjfmtCmEA9HuUC18KllpIMNsbogg1JQQJ99BDACi5YpzSofbjAAACAZAC3aT6");
+        options.UseFeatureFlags();
+    });
+    
+builder.Services.AddFeatureManagement();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
